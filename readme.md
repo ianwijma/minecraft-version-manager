beb3a37fb67a78d2863f8088d6a8e83d7f97f1e8
Ian Wijma <ian@wij.ma>
2023-02-12 11:23:14 +1100

    Added helpers + initial play command

--- readme.md
+++ readme.md
@@ -24,3 +24,6 @@ $ npm publish
 
 MIT - see LICENSE
 
+# Authentication helpI'm using the GDLauncher as an example:
+- Trigger OAuth: https://github.com/gorilla-devs/GDLauncher/blob/17df6d8fc9c1505a6c216bac7321f25225174324/src/common/reducers/actions.js#L703
+- Handle Oauth: https://github.com/gorilla-devs/GDLauncher/blob/7fd15c7d75de8af1416c53c6df3acc69fc3b0fdb/public/electron.js#L429
---
+++ src/commands/play.ts
@@ -0,0 +1,23 @@
+import { GluegunCommand, GluegunToolbox } from 'gluegun'
+
+module.exports = {
+  name: 'play',
+  run: async (toolbox: GluegunToolbox) => {
+    const { print: { info } } = toolbox;
+
+    info('Authenticate');
+
+    // Authenticate
+
+    // Login
+
+    // Create mod pack
+
+    // Download mods
+
+    // Copy files
+
+    // Launch game
+  }
+
+} as GluegunCommand
